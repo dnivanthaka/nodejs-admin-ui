80b9ca52585e12bd343ec7023ed165e0c063b4d6
dnivanthaka <nivanthaka@gmail.com>
2016-08-03 23:02:33 +0530

    Updated sources with working example

--- server.js
+++ server.js
@@ -10,17 +10,42 @@ app.get('/', function(req, res){
     res.send('It works!!!');
 });
 
+//Init GPIO
+gpio.init();
+
 app.get('/gpio', function(req, res){
     //res.send('It works!!!');
-    gpio.init();
     //gpio.export(17);
-    console.log(gpio.get_exported());
+    console.log(JSON.stringify(gpio.get_exported()));
     gpio.dir(17, gpio.OUT);
     gpio.set(17, 1);
-    console.log(gpio.get_exported());
+    console.log(JSON.stringify(gpio.get_exported()));
     //console.log(gpio.GPIO.OUT);
     
-    res.end();
+    res.end(JSON.stringify(gpio.get_exported()));
+});
+app.get('/gpio/:id', function(req, res){
+    var pin = Number(req.params.id);
+    //gpio.export(17);
+    console.log(JSON.stringify(gpio.get_exported()));
+    gpio.dir(pin, gpio.OUT);
+    gpio.set(pin, 0);
+    console.log(JSON.stringify(gpio.get_exported()));
+    //console.log(gpio.GPIO.OUT);
+    
+    res.end(JSON.stringify(gpio.get_exported()));
+});
+app.get('/gpio/:id/:value', function(req, res){
+    var pin = Number(req.params.id);
+    var val = Number(req.params.value);
+    //gpio.export(17);
+    console.log(JSON.stringify(gpio.get_exported()));
+    gpio.dir(pin, gpio.OUT);
+    gpio.set(pin, val);
+    console.log(JSON.stringify(gpio.get_exported()));
+    //console.log(gpio.GPIO.OUT);
+    
+    res.end(JSON.stringify(gpio.get_exported()));
 });
 
 var server = app.listen(8081, function(){
